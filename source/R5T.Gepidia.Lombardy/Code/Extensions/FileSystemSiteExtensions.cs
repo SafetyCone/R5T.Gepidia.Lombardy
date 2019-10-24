72c1c7f66954d015d20bc1c8d36a6b3f9763d701
MinexAutomation <minex.automation@gmail.com>
2019-10-24 11:40:04 -0700

    Added FileSystemSite extension method.

---
+++ source/R5T.Gepidia.Lombardy/Code/Extensions/FileSystemSiteExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using R5T.Lombardy;
+
+
+namespace R5T.Gepidia.Lombardy.Extensions
+{
+    public static class FileSystemSiteExtensions
+    {
+        public static FileSystemSite EnsureSiteDirectoryPathIsDirectoryIndicated(this FileSystemSite site, IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            var directoryPathIsDirectoryIndicated = stringlyTypedPathOperator.IsDirectoryIndicatedPath(site.DirectoryPath);
+            if (directoryPathIsDirectoryIndicated)
+            {
+                return site;
+            }
+            else
+            {
+                var directoryIndicatedDirectoryPath = stringlyTypedPathOperator.EnsureDirectoryPathIsDirectoryIndicated(site.DirectoryPath);
+
+                var output = new FileSystemSite(directoryIndicatedDirectoryPath, site.FileSystemOperator);
+                return output;
+            }
+        }
+    }
+}
