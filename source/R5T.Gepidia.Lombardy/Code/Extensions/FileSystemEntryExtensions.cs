7bc12e74547ff884d85d55001f50fc8191e275d5
MinexAutomation <minex.automation@gmail.com>
2019-12-11 12:18:46 -0800

    Added extension to standardize path formatting for file-system entries.

---
+++ source/R5T.Gepidia.Lombardy/Code/Extensions/FileSystemEntryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using R5T.Lombardy;
+
+
+namespace R5T.Gepidia.Lombardy.Extensions
+{
+    public static class FileSystemEntryExtensions
+    {
+        public static FileSystemEntry GetStandardPathFormatEntry(this FileSystemEntry fileSystemEntry, IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            var standardPath = stringlyTypedPathOperator.EnsureWindowsPath(fileSystemEntry.Path);
+
+            var standardPathFileSystemEntry = new FileSystemEntry(standardPath, fileSystemEntry.Type, fileSystemEntry.LastModifiedUTC);
+            return standardPathFileSystemEntry;
+        }
+    }
+}
